bc96434d33672f1a7b663b08333a36ccc58a24e9
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-01-13 15:17:38 -0300

    Exercise done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,14 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+       {
+        Console.Write("Please, insert your first name: ");
+        string first = Console.ReadLine();
+
+        Console.Write("Please, insert your last name: ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
+
 }
