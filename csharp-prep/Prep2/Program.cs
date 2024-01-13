dfff69d207ad8ab228181f23f0b79fca2de656b7
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2024-01-13 15:58:41 -0300

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please, insert your grade percentage: ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        string final_grade = "";
+
+        if (percent >= 90) {
+            final_grade = "A";
+        }
+
+        else if (percent >= 80) {
+            final_grade = "B";
+        }
+        
+        else if (percent >= 70) {
+            final_grade = "C";
+        }
+
+         else if (percent >= 60) {
+            final_grade = "D";
+        }
+
+         else {
+            final_grade = "F";
+         }
+
+        Console.WriteLine($"Your final grade is {final_grade}");
+
+        if (percent >= 70) {
+            Console.WriteLine("You passed! Congratulations!!!");
+        }
+
+        else {
+            Console.WriteLine("You have not been approved. Please contact your teacher.");
+        }
     }
 }
