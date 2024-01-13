3ecb0d4dba4296b3078eec654a3018a405cd2533
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2024-01-13 16:23:36 -0300

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       //  REQUEST 1 and 2
+        
+        // Console.Write($"Insert the magic number: ");
+        // int magic = int.Parse(Console.ReadLine());
+
+        // REQUEST 3
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != magic) {
+            Console.Write("Insert your guess: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magic > guess) {
+                Console.WriteLine("Higher!!");
+            }
+
+            else if (magic < guess) {
+                Console.WriteLine("Lower!!");
+            }
+
+            else {
+                Console.WriteLine("You guess it RIGT!!!! CONGRATULATIONS!!!!");
+            }
+        }
     }
 }
