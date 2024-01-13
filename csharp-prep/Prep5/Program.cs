d5e89fad2bbd1e9b101f5bfa391cf29a408b6fc0
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2024-01-13 17:04:56 -0300

    Update Program.cs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+       DisplayWelcome();
+
+       string userName = PromptUserName();
+       int userNumber = PromptUserNumber();
+
+       int squareNumber = SquaredNumber(userNumber);
+
+       DisplayResult(userName, squareNumber);
+    }
+
+    static void DisplayWelcome() {
+        Console.WriteLine("WELCOME TO OUR SYSTEM!");
+    }
+
+    static string PromptUserName() {
+        Console.Write("Please, insert your first name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber() {
+        Console.Write("Please insert your number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquaredNumber(int number) { 
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square) {
+        Console.WriteLine($"Your name is {name} and your number squared is {square}");
     }
 }
