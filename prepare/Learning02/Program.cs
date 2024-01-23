74c701306cf6231ab54f365da879265fdb6e2796
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2024-01-23 14:11:11 -0300

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,28 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
+    static void Main(string[] args) {
+       
+       Job job1 = new Job();
+       job1._jobOcupation = "Software Developer";
+       job1._companyName = "Softplan";
+       job1._salary = 12345.65;
+       job1._yearsOfWorking = 4;
+
+       Job job2 = new Job();
+       job2._jobOcupation = "Software Architeture";
+       job2._companyName = "Porsche";
+       job2._salary = 14235.94;
+       job2._yearsOfWorking = 7;
+
+       Resume myResume = new Resume();
+       myResume._name = "Lucas";
+
+       myResume._jobs.Add(job1);
+       myResume._jobs.Add(job2);
+
+       myResume.Display();
+
+
     }
 }
