0e4bdd43cfc37aa100848a1f91a69d3d54810046
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-09-21 23:44:40 -0300

    Prep. 4 - complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = 0;
+        List<int> numbers = new List<int>();
+
+        Console.Write("Enter number: ");
+        string userInput = Console.ReadLine();
+        number = int.Parse(userInput);
+
+        while (number != 0)
+        {
+            numbers.Add(number);
+
+            Console.Write("Enter number: ");
+            userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+        }
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Maximum: {max}");
     }
 }
