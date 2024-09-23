320a20db58ce56e8db53d1d0dbf9e7df4a32e745
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-09-23 18:37:50 -0300

    Job - part 1

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "KIS Solutions";
+        job1._startYear = 2024;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();  
+        job2._jobTitle = "Physics Teacher";
+        job2._company = "Êxito - Acompanhamento Escolar";
+        job2._startYear = 2014;
+        job2._endYear = 2023;
     }
 }
